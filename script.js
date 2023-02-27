6f530cde1d0303a82f7a3e6d744f13a0a35372d4
Meehdi99 <mhalouardi@gmail.com>
2023-02-27 21:23:10 +0100

    Add working script

--- script.js
+++ script.js
@@ -1,20 +1,79 @@
+// Add 0 to the default display
+// fix the dot spam
+// Add interaction with keys
+// Optional : color the selected operator
+// Add footer o l3ibat
+
+let a = 0,
+  op = "",
+  b = 0;
+
 const displayable = document.querySelectorAll(".dis");
 const clear = document.querySelector(".clear");
 const disCurrent = document.querySelector(".display");
+const operation = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const del = document.querySelector(".delete");
 
-clear.addEventListener("click", () => location.reload());
+del.addEventListener("click", () => deletion());
 
-displayable.forEach((button) =>
-  button.addEventListener("click", () => show(button.innerHTML))
+clear.addEventListener(
+  "click",
+  () => ((disCurrent.textContent = ""), (a = b = 0))
 );
 
-function show(dis) {
-  disCurrent.textContent += dis;
+operation.forEach((button) =>
+  button.addEventListener("click", () => operate(button.textContent))
+);
+
+displayable.forEach((button) =>
+  button.addEventListener("click", () => show(button.textContent))
+);
+
+equals.addEventListener("click", () => calculate());
+
+function deletion() {
+  if (!(disCurrent.textContent == "")) {
+    disCurrent.textContent = disCurrent.textContent.slice(0, -1);
+  }
 }
 
-// const operate = function (callback, a, b) {};
+function show(dis) {
+  if (disCurrent.textContent.length < 8) {
+    if ((dis >= 0 && dis <= 9) || dis == ".") disCurrent.textContent += dis;
+  }
+}
 
-// const display = function (args) {};
+function operate(operator) {
+  if (op == "") a = Number(disCurrent.textContent);
+  op = operator;
+  disCurrent.textContent = "";
+}
+
+function calculate() {
+  b = Number(disCurrent.textContent);
+  switch (op) {
+    case "+":
+      display(add(a, b));
+      break;
+    case "-":
+      display(subtract(a, b));
+      break;
+    case "x":
+      display(multiply(a, b));
+      break;
+    case "/":
+      display(divide(a, b));
+      break;
+  }
+  a = disCurrent.textContent;
+  op = "";
+}
+
+function display(res) {
+  if (res.toString().length < 8) disCurrent.textContent = res;
+  else disCurrent.textContent = res.toExponential(2);
+}
 
 const add = function (a, b) {
   return a + b;
