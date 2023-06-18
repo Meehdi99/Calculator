36f8dbb5d085301186e3a33015681575006e01a5
Meehdi99 <mhalouardi@gmail.com>
2023-06-18 12:11:47 +0100

    visit to clean

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let a = 0,
 
 const operation = document.querySelectorAll(".operator");
 const displayable = document.querySelectorAll(".dis");
-const currentDis = document.querySelector(".display");
+const currentDisplay = document.querySelector(".display");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
 const del = document.querySelector(".delete");
@@ -13,21 +13,19 @@ const dot = document.querySelector(".dot");
 window.addEventListener("keydown", keysInput);
 
 dot.addEventListener("click", () => {
-  if (!currentDis.textContent.includes(".")) showDis(".");
+  if (!currentDisplay.textContent.includes(".")) showDisplay(".");
 });
 
 del.addEventListener("click", () => {
-  if (!(currentDis.textContent == "0")) {
-    currentDis.textContent = currentDis.textContent.slice(0, -1);
-    if (currentDis.textContent.length == 0) {
-      currentDis.textContent = 0;
-    }
+  currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+  if (currentDisplay.textContent.length == 0) {
+    currentDisplay.textContent = 0;
   }
 });
 
 clear.addEventListener(
   "click",
-  () => ((currentDis.textContent = "0"), (a = b = 0))
+  () => ((currentDisplay.textContent = "0"), (a = b = 0), (op = ""))
 );
 
 operation.forEach((button) =>
@@ -35,57 +33,58 @@ operation.forEach((button) =>
 );
 
 displayable.forEach((button) =>
-  button.addEventListener("click", () => showDis(button.textContent))
+  button.addEventListener("click", () => showDisplay(button.textContent))
 );
 
 equals.addEventListener("click", () => {
-  b = Number(currentDis.textContent);
+  b = Number(currentDisplay.textContent);
   calculate();
-  a = Number(currentDis.textContent);
+  a = Number(currentDisplay.textContent);
   op = "";
 });
 
 function operate(operator) {
-  if (op == "") a = Number(currentDis.textContent);
+  if (!op) a = Number(currentDisplay.textContent);
   else {
     equals.click();
   }
-  currentDis.textContent = "";
+  currentDisplay.textContent = "";
   op = operator;
 }
 
 function calculate() {
   switch (op) {
     case "+":
-      displayRes(add(a, b));
+      displayResult(add(a, b));
       break;
     case "-":
-      displayRes(subtract(a, b));
+      displayResult(subtract(a, b));
       break;
     case "×":
-      displayRes(multiply(a, b));
+      displayResult(multiply(a, b));
       break;
     case "÷":
-      displayRes(divide(a, b));
+      displayResult(divide(a, b));
       break;
   }
 }
 
-function showDis(dis) {
-  if (currentDis.textContent != "0" || dis == ".") {
-    if (currentDis.textContent.length < 8) {
-      if ((dis >= 0 && dis <= 9) || dis == ".") currentDis.textContent += dis;
+function showDisplay(display) {
+  if (currentDisplay.textContent != "0" || display == ".") {
+    if (currentDisplay.textContent.length < 8) {
+      if ((display >= 0 && display <= 9) || display == ".")
+        currentDisplay.textContent += display;
     }
-  } else currentDis.textContent = dis;
+  } else currentDisplay.textContent = display;
 }
 
-function displayRes(res) {
-  if (res.toString().length < 8) currentDis.textContent = res;
-  else currentDis.textContent = res.toExponential(2);
+function displayResult(result) {
+  if (result.toString().length < 8) currentDisplay.textContent = result;
+  else currentDisplay.textContent = result.toExponential(2);
 }
 
 function keysInput(e) {
-  if (e.key >= 0 && e.key <= 9) showDis(e.key);
+  if (e.key >= 0 && e.key <= 9) showDisplay(e.key);
   if (e.key === ".") dot.click();
   if (e.key === "=" || e.key === "Enter") equals.click();
   if (e.key === "Backspace") del.click();
